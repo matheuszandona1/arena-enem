07cec0dfb186b57092b60ccd4787018ae6c145bd
mastronelli88@gmail.com <mastronelli88@gmail.com>
2024-04-09 12:34:28 -0300

    vercel

--- .dev/dist/res/js/index.739bf03c.js
+++ .dev/dist/res/js/index.739bf03c.js
@@ -806,6 +806,30 @@ $(document).ready(function() {
         ]
     });
 });
+$("html").mousemove(function(e) {
+    var wx = $(window).width();
+    var wy = $(window).height();
+    var x = e.pageX - this.offsetLeft;
+    var y = e.pageY - this.offsetTop;
+    var newx = x - wx / 2;
+    var newy = y - wy / 2;
+    $(".hero .position-absolute").each(function() {
+        var speed = $(this).attr("data-speed");
+        if ($(this).attr("data-revert")) speed *= -1;
+        TweenMax.to($(this), 1, {
+            x: 1 - newx * speed,
+            y: 1 - newy * speed
+        });
+    });
+    $(".dep .position-absolute").each(function() {
+        var speed = $(this).attr("data-speed");
+        if ($(this).attr("data-revert")) speed *= -1;
+        TweenMax.to($(this), 1, {
+            x: 1 - newx * speed,
+            y: 1 - newy * speed
+        });
+    });
+});
 
 },{}]},["l9Mez","ebWYT"], "ebWYT", "parcelRequirec618")
 
